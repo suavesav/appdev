6574d094ae83099d50269dca1520c8b31d48558e
Savinay <snangali@purdue.edu>
2015-01-01 19:09:56 +0300

    A simple button with View.OnClickListener

--- src/com/example/bookApp1/BookAppActivity1.java
+++ src/com/example/bookApp1/BookAppActivity1.java
@@ -7,31 +7,31 @@ import android.widget.Button;
 
 import java.util.Date;
 
-public class BookAppActivity1 extends Activity{
+public class BookAppActivity1 extends Activity implements View.OnClickListener{
     /**
      * Called when the activity is first created.
      */
 
-//    Button btn;
+    Button btn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-//        btn = (Button)findViewById(R.id.button);
-//        btn.setOnClickListener(this);
-//        updateTime();
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(this);
+        updateTime();
     }
-//
-//    public void onClick(View view)
-//    {
-//        updateTime();
-//    }
-//
-//    private void updateTime()
-//    {
-//        btn.setText(new Date().toString());
-//    }
+
+    public void onClick(View view)
+    {
+        updateTime();
+    }
+
+    private void updateTime()
+    {
+        btn.setText(new Date().toString());
+    }
 
 }
