a8d4b17d8126e58581415ac10e65179671b68b3f
Savinay <snangali@purdue.edu>
2015-01-29 11:43:53 -0500

    w/ java code this time

--- src/com/example/bookApp1/BookAppActivity1.java
+++ src/com/example/bookApp1/BookAppActivity1.java
@@ -2,8 +2,15 @@ package com.example.bookApp1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class BookAppActivity1 extends Activity{
+public class BookAppActivity1 extends Activity implements RadioGroup.OnCheckedChangeListener{
+    RadioGroup orientation;
+    RadioGroup gravity;
     /**
      * Called when the activity is first created.
      */
@@ -13,5 +20,31 @@ public class BookAppActivity1 extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        orientation = (RadioGroup)findViewById(R.id.orientation);
+        orientation.setOnCheckedChangeListener(this);
+        gravity = (RadioGroup)findViewById(R.id.gravity);
+        gravity.setOnCheckedChangeListener(this);
+    }
+
+    public void onCheckedChanged(RadioGroup group, int checkedId)
+    {
+        switch (checkedId)
+        {
+            case(R.id.grp1h):
+                orientation.setOrientation(LinearLayout.HORIZONTAL);
+                break;
+            case(R.id.grp1v):
+                orientation.setOrientation(LinearLayout.VERTICAL);
+                break;
+            case(R.id.grp2l):
+                gravity.setGravity(Gravity.LEFT);
+                break;
+            case(R.id.grp2c):
+                gravity.setGravity(Gravity.CENTER);
+                break;
+            case(R.id.grp2r):
+                gravity.setGravity(Gravity.RIGHT);
+                break;
+        }
     }
 }
