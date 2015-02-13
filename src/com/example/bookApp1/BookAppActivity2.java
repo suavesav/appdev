f889d1bcd8d999339d1a74fd1f964d10c13fc6f0
Savinay <snangali@purdue.edu>
2015-02-12 21:09:37 -0500

    closing on activity change

--- src/com/example/bookApp1/BookAppActivity2.java
+++ src/com/example/bookApp1/BookAppActivity2.java
@@ -7,14 +7,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.app.ListActivity;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
-public class BookAppActivity2 extends Activity{
+public class BookAppActivity2 extends ListActivity{
+    private TextView selection;
+    private static final String[] items= {"Find Location", "Academic", "Residence", "Sports", "Fountain Run"};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen2);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+        selection = (TextView)findViewById(R.id.selection);
 
         Button buttonBack = (Button) findViewById(R.id.back);
         buttonBack.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +32,10 @@ public class BookAppActivity2 extends Activity{
                 startActivity(new Intent(getApplicationContext(), BookAppActivity1.class));
             }
         });
+    }
+
+    public void onListItemClick(ListView parent, View v, int position, long id)
+    {
+        selection.setText(items[position]);
     }
 }
