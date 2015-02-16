28c9dd8ecf10e7026f2b6692d382226692164685
Savinay <snangali@purdue.edu>
2015-02-16 13:10:06 -0500

    Initial bluetooth tests

--- src/com/example/bookApp1/BookAppActivity1.java
+++ src/com/example/bookApp1/BookAppActivity1.java
@@ -2,18 +2,16 @@ package com.example.bookApp1;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.*;
 
-import java.text.MessageFormat;
 
-public class BookAppActivity1 extends Activity implements RadioGroup.OnCheckedChangeListener{
-    RadioGroup orientation;
-    RadioGroup gravity;
+public class BookAppActivity1 extends Activity{
+    private static final int REQUEST_ENABLE_BT = 3;
     /**
      * Called when the activity is first created.
      */
@@ -23,10 +21,19 @@ public class BookAppActivity1 extends Activity implements RadioGroup.OnCheckedCh
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        orientation = (RadioGroup)findViewById(R.id.orientation);
-        orientation.setOnCheckedChangeListener(this);
-        gravity = (RadioGroup)findViewById(R.id.gravity);
-        gravity.setOnCheckedChangeListener(this);
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null)
+        {
+            startActivity(new Intent(getApplicationContext(), BookAppActivity2.class));
+            finish();
+        }
+
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
         Button buttonNext = (Button) findViewById(R.id.next);
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,28 +45,19 @@ public class BookAppActivity1 extends Activity implements RadioGroup.OnCheckedCh
         showAlert();
     }
 
-    public void onCheckedChanged(RadioGroup group, int checkedId)
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        switch (checkedId)
+        if(requestCode == REQUEST_ENABLE_BT)
         {
-            case(R.id.grp1h):
-                orientation.setOrientation(LinearLayout.HORIZONTAL);
-                break;
-            case(R.id.grp1v):
-                orientation.setOrientation(LinearLayout.VERTICAL);
-                break;
-            case(R.id.grp2l):
-                gravity.setGravity(Gravity.LEFT);
-                break;
-            case(R.id.grp2c):
-                gravity.setGravity(Gravity.CENTER);
-                break;
-            case(R.id.grp2r):
-                gravity.setGravity(Gravity.RIGHT);
-                break;
+            if(resultCode == RESULT_OK)
+                showAlert();
+            else if(resultCode == RESULT_CANCELED)
+            {
+                startActivity(new Intent(getApplicationContext(), BookAppActivity2.class));
+                finish();
+            }
         }
     }
-
     public void showAlert()
     {
         new AlertDialog.Builder(this)
