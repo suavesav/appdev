161b1679a821654ba347d11aa1a2848c1c374b78
Savinay <snangali@purdue.edu>
2015-03-05 14:02:27 -0500

    messed that version up

--- src/com/example/bookApp1/ConnectionActivity.java
+++ src/com/example/bookApp1/ConnectionActivity.java
@@ -22,38 +22,17 @@ import java.util.ArrayList;
  */
 public class ConnectionActivity extends Activity {
 
-
+    private ArrayList<BluetoothDevice> discoveredDevices = new ArrayList<BluetoothDevice>();
+    private BluetoothAdapter mBluetoothAdapter;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connection);
 
         //Getting the bluetooth adapter and starting discovery
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        final ArrayList<String> discoveredDevices = new ArrayList<String>();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         ListView lv = (ListView) findViewById(R.id.btList);
 
-
-        //Getting the broadcast receiver
-        final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            //What to do when you receive a bcast
-            public void onReceive(Context context, Intent intent) {
-                //if new bt device found
-                String action = intent.getAction();
-//                if(BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action))
-//                {}
-//                else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
-//                {}
-                if(BluetoothDevice.ACTION_FOUND.equals(action))
-                {
-                    //get the device information and store it in an arrayAdapter to display
-                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    discoveredDevices.add(device.getName());
-                    showToast(device);
-                }
-            }
-        };
         IntentFilter filter = new IntentFilter();
         filter.addAction(BluetoothDevice.ACTION_FOUND);
         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
@@ -63,7 +42,7 @@ public class ConnectionActivity extends Activity {
         registerReceiver(mBroadcastReceiver, filter);
 
         mBluetoothAdapter.startDiscovery();
-        final ArrayAdapter<String> btDevs = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, discoveredDevices);
+        final ArrayAdapter<BluetoothDevice> btDevs = new ArrayAdapter<BluetoothDevice>(this, android.R.layout.simple_list_item_1, discoveredDevices);
         lv.setAdapter(btDevs);
 
         //Temporary
@@ -74,8 +53,17 @@ public class ConnectionActivity extends Activity {
                 startActivity(new Intent(getApplicationContext(), BookAppActivity2.class));
             }
         });
+
+        Button buttonScan = (Button) findViewById(R.id.scanner);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {
+                mBluetoothAdapter.startDiscovery();
+            }
+        });
         //CHECK IF CONNECTION ESTABLISHED and if so GO TO LIST
     }
+
 
     public void showToast(BluetoothDevice dev)
     {
@@ -83,4 +71,29 @@ public class ConnectionActivity extends Activity {
                 .makeText(this, dev.getName(), Toast.LENGTH_SHORT)
                 .show();
     }
+
+
+    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        //What to do when you receive a bcast
+        public void onReceive(Context context, Intent intent) {
+            //if new bt device found
+            String action = intent.getAction();
+            if(BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action))
+            {
+                discoveredDevices = new ArrayList<BluetoothDevice>();
+            }
+            else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
+            {
+
+            }
+            else if(BluetoothDevice.ACTION_FOUND.equals(action))
+            {
+                //get the device information and store it in an arrayAdapter to display
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                discoveredDevices.add(device);
+                showToast(device);
+            }
+        }
+    };
 }
